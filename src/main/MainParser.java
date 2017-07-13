64487134468830a386f7aaa5cec5ba66ba85d575
talitz <talyitzhak100@gmail.com>
2017-07-13 23:17:44 +0300

    completed all tasks!

--- src/main/MainParser.java
+++ src/main/MainParser.java
@@ -19,12 +19,14 @@ public class MainParser {
 		accountInfoDocument = Jsoup.parse(accountInfo, "UTF-8", "");
 	}
 	
+	//Return the account number and name.
 	public static void taskOne() {
 		Element body = accountInfoDocument.body();
 		System.out.println("Account Nubmber: "+body.getElementById("accountNumber").text());
 		System.out.println("Account Name: "+body.getElementById("AccountName").text());
 	}
 	
+	//Check if the account status is active, and if so return the account balance.
 	public static void taskTwo() {
 		accountInfoDocument.getAllElements().forEach(element -> {
 			if(element.text().equals("ACTIVE (Payments are allowed)")) {
@@ -33,11 +35,13 @@ public class MainParser {
 		});	
 	}
 	
+	//Load the “Payment History” page.
 	public static void taskThree() throws IOException {
 		File paymentHistory = new File(Constants.PaymentHistoryFile);
 		paymentHistoryDocument = Jsoup.parse(paymentHistory, "UTF-8", "");
 	}
 	
+	//From the “Bill records” table return the bill amount and due date for November 2015.
 	public static void taskFour(String year, String month) {
 		Element body = paymentHistoryDocument.body();
 		Element table = body.getElementById("billsTable");
@@ -57,6 +61,7 @@ public class MainParser {
 		}
 	}
 	
+	//From the “payment records” table return the payment amount and date from the last week (assumption: today is the 1.1.2016)
 	public static void taskFive(LocalDate today,int weeksBefore) {
 		Element body = paymentHistoryDocument.body();
 		Element table = body.getElementById("PaymentsTable");
@@ -82,8 +87,27 @@ public class MainParser {
 		}
 	}
 	
-	public static void taskSix() {
-
+	//Calculate the sum of all payments made (e.g. applied payments) in November 2015.
+	public static void taskSix(String year, String month) {
+		Element body = paymentHistoryDocument.body();
+		Element table = body.getElementById("PaymentsTable");
+		Elements rows = table.select("tr");
+		double sumOfAllPaymentAmounts = 0;
+		for (int i = 1; i < rows.size(); i++) { //first row is the col names so skip it.
+		    Element row = rows.get(i);
+		    Elements cols = row.select("td");
+		    
+		    String billDate = cols.first().text();
+		    if(billDate.matches(Constants.dateRegularExpression)) { //if bill date is of date syntax
+		    	String[] splitedDate = billDate.split("/");
+		    	String paymentStatus = cols.get(3).text();
+		    	if(splitedDate[0].equals(month) && splitedDate[2].equals(year) && paymentStatus.equals("Applied")) {
+		    		String paymentAmount = cols.get(1).text().substring(1);
+		    		sumOfAllPaymentAmounts += Double.valueOf(paymentAmount);
+		    	}
+		    }
+		}
+		System.out.println("Sum of all payments made in Year: "+year+ " and Month: "+month + " is: "+sumOfAllPaymentAmounts);
 	}
 	
 	
@@ -94,7 +118,7 @@ public class MainParser {
 		taskThree();
 		taskFour("2015","11");
 		taskFive(LocalDate.of(2016,Month.JANUARY,1),1); //assumptions given in the assignment, today is 1/1/2016, and we want all data 1 week before that date
-		taskSix();
+		taskSix("2015","11");
 	}
 
 }
